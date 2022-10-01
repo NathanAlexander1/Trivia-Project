57d0deb3e210971f8c327f8fbd8441fad2b7019c
NathanAlexander1 <72030740+NathanAlexander1@users.noreply.github.com>
2022-10-01 16:47:32 -0700

    got dynamic buttons to show upon click - bug: the question and answer choices displayed are all the last ones in my lists

--- assets/script.js
+++ assets/script.js
@@ -6,65 +6,97 @@ var startBtn = document.querySelector("#start-button");
 var timerCountdownSpan = document.querySelector("#time-left");
 var questionSpan = document.querySelector("#question");
 
+
+
 //starting score
 var score = 0;
 //timer variable
 var timeLeft = 5;
 //set time interval variable to attach to timer later
 var timeInterval;
-
 var isPlaying = false;
+var arrayOfQuesObj = 
+[{
+    question: "How many continents are there?",
+    possibleAnswers: ["Ten", "Seven", "Eight", "Nine"],
+    correctAnswer: "Seven"
+}, 
+{
+    question: "What color is the sky?",
+    possibleAnswers: ["Green", "Orange", "Blue", "Pumpkin"],
+    correctAnswer: "Blue"
+},
+{
+    question: "How much wood could a woodchuck chuck?",
+    possibleAnswers: [11, 15, 7, 9],
+    correctAnswer: 11
+},
+{
+    question: "What is the best thing to drink from?",
+    possibleAnswers: ["Plate", "Bowl", "Spoon", "Cup"],
+    correctAnswer: "Cup"
+}];
+
+
 
 //add event listener to start button
 startBtn.addEventListener("click", function () {
     if (isPlaying) {
-        console.log("not playing");
+        // console.log("not playing");
         return;
     }
     isPlaying = true
-    console.log("game started!");
-    questionSpan.textContent = "How should I figure out how to cycle through questions?"
+    // console.log("game started!");
+    answerList ();
     timerCountdownSpan.textContent = timeLeft;
 
     timeInterval = setInterval(function () {
         if (timeLeft > 0) {
             timerCountdownSpan.textContent = timeLeft;
             timeLeft--;
-            console.log(timerCountdownSpan);
             
-        } else {
-            console.log("game over!");
+        } else if (timeLeft = 0) {
+            console.log("ALL DONE")
             isPlaying = false;
             
         }
     }, 1000)
         
-    
+
 })
-    //when clicked call timer function
-    //when clicked, call display function
 
-//function timer
-    //timer-- (refer back to activities)
-
-//function to display:
-// var userSelect; doesnt need to equal anything (or maybe make it equal to a click event)
-        //first picture (array[0].image)
-            //document.queryselector(image) - set image equal to celebrity eyeball (setAttribute)
-        //four answer options
-// for loop(looping over array of celebrities)
-        //select ul var ul = doc.querySelector
-            //var li = document.createElement("li")
-            //var x = document.createElement("button")
-            // x.textContent = array[0].celebrities[0]
-            //li.appendChild(x)
-//ul.appendChild(li)
-
-// if userSelect on click === correct answer (somehow) 
+    function answerList () {
+        //user selection variable
+        var userSelection;
 
 
+        
+        //for array of objects[i]
+            //questionSpan.textContent = arr[i].question
+            //listForButtons.textContent = arr[i].possibleAnswers
+        //for each object in array
+        for (var i = 0; i < arrayOfQuesObj.length; i++) {
+            var ulForAnswers = document.querySelector("#listContainer");
+            //change question text to question from key of arrayOfQuestObj`
+            questionSpan.textContent = arrayOfQuesObj[i].question;
+            // console.log(questionSpan.textContent);
+            // create variable to access possible answer array of each object
+            var possAnsArr = arrayOfQuesObj[i].possibleAnswers;
+            // console.log(possAnsArr);
+            var listForButtons = document.createElement("li");
+            //attach list items to ul
+            ulForAnswers.appendChild(listForButtons);
+            console.log(ulForAnswers);
 
+            var listButton = document.createElement("button");
+            listForButtons.appendChild(listButton);
 
-//user guesses option
-    //if that option is correct, display "great job"
-    //add one to score
+            //iterate over array of possible answers
+                for (var x = 0; x < possAnsArr.length; x++) {
+                    listButton.textContent = possAnsArr[x];
+                }
+           
+            var correctAnswer = arrayOfQuesObj[i].correctAnswer;
+            // console.log(correctAnswer);
+        }
+    }
