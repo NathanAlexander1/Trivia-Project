11220cdde5fe3c34f5d15717cf7a78c7164c8a7c
NathanAlexander1 <72030740+NathanAlexander1@users.noreply.github.com>
2022-10-01 10:26:42 -0700

    added base files

---
+++ assets/script.js
@@ -0,0 +1,48 @@
+console.log('tedfadfa');
+// create score variable (globally)
+//timer variable
+// 
+//var array of objects (add m):
+// 
+// [
+//     {
+//         celebrities: [1,2,3,4],
+//         image:'',
+//         correctCeleb: 1
+//     },    
+//      {
+//         celebrities: [1,2,3,4],
+//         image:'',
+//         correctCeleb: 1
+//     }
+// 
+// ]
+
+//add event listener to start button
+    //when clicked call timer function
+    //when clicked, call display function
+
+//function timer
+    //timer-- (refer back to activities)
+
+//function to display:
+// var userSelect; doesnt need to equal anything (or maybe make it equal to a click event)
+        //first picture (array[0].image)
+            //document.queryselector(image) - set image equal to celebrity eyeball (setAttribute)
+        //four answer options
+// for loop(looping over array of celebrities)
+        //select ul var ul = doc.querySelector
+            //var li = document.createElement("li")
+            //var x = document.createElement("button")
+            // x.textContent = array[0].celebrities[0]
+            //li.appendChild(x)
+//ul.appendChild(li)
+
+// if userSelect on click === correct answer (somehow) 
+
+
+
+
+//user guesses option
+    //if that option is correct, display "great job"
+    //add one to score
